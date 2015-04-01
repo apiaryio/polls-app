9e5c736a71c93f365016ab6f975b97694d7b49a4
Kyle Fuller <kyle@fuller.li>
2015-04-01 21:06:47 +0100

    Initial Commit

---
+++ Polls/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Polls
+//
+//  Created by Kyle Fuller on 01/04/2015.
+//  Copyright (c) 2015 Apiary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
